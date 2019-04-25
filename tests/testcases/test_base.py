4461aad5ce8dcdce9c1f2fa796a0089c8336529a
Truong Pham <vantruongt2@gmail.com>
2019-04-25 08:54:58 +0700

    Update core

--- tests/testcases/test_base.py
+++ tests/testcases/test_base.py
@@ -1,17 +1,21 @@
 import unittest
 import pytest
 
-from support.factory import start_driver, maximize_browser, close_all_browsers, navigate
+from selenpy.support import browser
+from selenpy.support.conditions import have
+import logging
 
 
 class TestBase(unittest.TestCase):
-    
+
     @pytest.fixture(scope="session", autouse=True)
-    def setup(self):        
-        start_driver("chrome")
-        maximize_browser()
-        navigate("https://google.com")    
-        
+    def setup(self):
+        logging.info("Starting the test on " + str(pytest.browser_name))                
+        browser.start_driver(pytest.browser_name, pytest.remote_host)
+        browser.maximize_browser()
+        browser.open_url("https://google.com")
+        browser.wait_until(have.title("Google"))
+
         # Close all browsers when tests have been finished
         yield        
-        close_all_browsers()        
+        browser.quit_all_browsers()        
